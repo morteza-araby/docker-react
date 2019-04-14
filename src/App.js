73cc1e984e38daefe700c2e7d0486ffa48b67dd3
Morteza Araby <morteza.araby@gmail.com>
2019-04-14 17:22:45 +0200

    update for docker hup push

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello World!! For Testing Azure CI
+            Hello World!! For Testing Docker Hub
           </p>
           <a
             className="App-link"
